aad3b645f77bc3267863b1c087b8053bf32648b2
Md. Asif Rahman <asif@unlocklive.com>
2023-11-10 02:11:17 +0600

    Circular linked list's deleteNode() method issue fixed

--- GoLang/06 Linked List/03_Circular_Linked_List/circularLinkedList.go
+++ GoLang/06 Linked List/03_Circular_Linked_List/circularLinkedList.go
@@ -12,92 +12,110 @@ type CircularLinkedList struct {
 }
 
 // Insert at the beginning of the circular linked list
-func (list *CircularLinkedList) InsertAtBeginning(data int) {
+func (list *CircularLinkedList) insertAtBeginning(data int) {
 	newNode := &Node{
 		data: data, 
-		next: nil,
+		next: list.head,
 	}
 
 	if list.head == nil {
 		newNode.next = newNode // Make the node point to itself to form a circular list
 		list.head = newNode
-	} else {
-		current := list.head
-		for current.next != list.head {
-			current = current.next
-		}
+		return
+	} 
+
+	current := list.head
+	for current.next != list.head {
+		current = current.next
+	}
+
+		current.next = newNode
 		current.next = newNode
 		newNode.next = list.head
-		list.head = newNode 
-	}
+	current.next = newNode
+		newNode.next = list.head
+	list.head = newNode 
 }
 
 // Insert at the end of the circular linked list
-func (list *CircularLinkedList) InsertAtEnd(data int) {
-	newNode := &Node{data: data, next: nil}
+func (list *CircularLinkedList) insertAtEnd(data int) {
+	newNode := &Node{
+		data: data, 
+		next: list.head,
+	}
 
 	if list.head == nil {
 		newNode.next = newNode // Make the node point to itself to form a circular list
 		list.head = newNode
-	} else {
-		current := list.head
-		for current.next != list.head {
-			current = current.next
-		}
-		current.next = newNode
-		newNode.next = list.head
+		return
+	} 
+
+	current := list.head
+	for current.next != list.head {
+		current = current.next
 	}
+	current.next = newNode
 }
 
 // Insert after a given node in the circular linked list
-func (list *CircularLinkedList) InsertAfter(prevNode *Node, data int) {
-	if prevNode == nil {
+func (list *CircularLinkedList) insertAfter(node *Node, data int) {
+	if node == nil {
 		fmt.Println("Invalid node provided")
 		return
 	}
 
-	newNode := &Node{data: data, next: nil}
-	newNode.next = prevNode.next
-	prevNode.next = newNode
-
-	if prevNode == list.head { // Update head if inserting after the head node
-		list.head = newNode
+	newNode := &Node{
+		data: data, 
+		next: node.next,
 	}
+
+	node.next = newNode
 }
 
 // Delete a node from the circular linked list
-func (list *CircularLinkedList) DeleteNode(data int) {
+func (list *CircularLinkedList) deleteNode(data int) bool {
 	if list.head == nil {
 		fmt.Println("Circular linked list is empty.")
-		return
+		return false
 	}
 
+	deleted := false
 	current := list.head
 	var prev *Node
 
+	// Find the node to be deleted and its previous node
 	for {
 		if current.data == data {
-			if current == list.head { // If the node to be deleted is the head
-				list.head = current.next
-				if current.next == current { // If there's only one node in the list
-					list.head = nil
-				} else {
-					prev.next = current.next
+			// Handle the case when the node to be deleted is the only node in the list
+			if current == list.head && current.next == list.head {
+				list.head = nil
+			} else if current == list.head {
+				// Handle the case when the node to be deleted is the head of the list
+				prev = list.head
+				for prev.next != list.head {
+					prev = prev.next
 				}
+				prev.next = current.next
+				list.head = current.next
 			} else {
+				// General case: update the next pointer of the previous node to skip the current node
 				prev.next = current.next
 			}
+
+			// Set deleted = true when the node is found and deleted
+			deleted = true
+			break
+		}
+
+		if current.next == list.head {
 			break
 		}
 
 		prev = current
 		current = current.next
-
-		if current == list.head { // Node not found after traversing the entire list
-			fmt.Println("Node not found.")
-			break
-		}
 	}
+
+	return deleted
 }
 
 // Traverse the circular linked list
@@ -121,17 +139,19 @@ func (list *CircularLinkedList) traverse() {
 func main() {
 	list := CircularLinkedList{}
 
-	list.InsertAtBeginning(10)
-	list.InsertAtBeginning(20)
-	list.InsertAfter(list.head, 15)
-	list.InsertAtBeginning(30)
-	list.InsertAtBeginning(40)
-	list.InsertAfter(list.head, 25)
-	list.InsertAtEnd(70)
+	list.insertAtBeginning(10)
+	list.insertAfter(list.head, 25)
+	list.insertAtBeginning(20)
+	list.insertAfter(list.head, 15)
+	list.insertAtBeginning(30)
+	list.insertAtBeginning(40)
+	list.insertAtEnd(70)
 
 	list.traverse()
 
-	list.DeleteNode(30)
+	list.deleteNode(40)
+	list.deleteNode(25)
+	list.deleteNode(70)
 
 	list.traverse()
 }
